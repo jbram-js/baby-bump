7bd990b3b1e59687e98c11b204060aa2cc84d418
“Joe <josephbramwell@bolstersystems.com>
2021-02-28 09:12:13 +0000

    burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import "../styles/NavBar.css";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="nav">
-      <ul className="nav-link">
-        <li className="nav-link-item">
-          <Link to="/" className="link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-link-item">
-          <Link to="/about" className="link">
-            About us
-          </Link>
-        </li>
-        <li className="nav-link-item">
-          <Link to="/our-team" className="link">
-            Meet the team
-          </Link>
-        </li>
-        <li className="nav-link-item">
-          <Link to="/little-box-of-hope" className="link">
-            Little box of hope
-          </Link>
-        </li>
-        <li className="nav-link-item">
-          <Link to="/contact" className="link">
-            Contact us
-          </Link>
-        </li>
-      </ul>
+      <nav>
+        <div className="logo">Bury Bump Buddies</div>
+        <ul
+          className="nav-link"
+          style={{
+            transform: open ? "translateX(0px)" : "",
+          }}
+        >
+          <li className="nav-link-item">
+            <Link to="/" className="link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-link-item">
+            <Link to="/about" className="link">
+              About us
+            </Link>
+          </li>
+          <li className="nav-link-item">
+            <Link to="/our-team" className="link">
+              Meet the team
+            </Link>
+          </li>
+          <li className="nav-link-item">
+            <Link to="/little-box-of-hope" className="link">
+              Little box of hope
+            </Link>
+          </li>
+          <li className="nav-link-item">
+            <Link to="/contact" className="link">
+              Contact us
+            </Link>
+          </li>
+        </ul>
+        <i onClick={() => setOpen(!open)} className="fas fa-bars burger"></i>
+      </nav>
     </div>
   );
 };
