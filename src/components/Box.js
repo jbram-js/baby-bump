f867a0429ccaab6a5b050fcee5eaa8d81d20910f
“Joe <josephbramwell@bolstersystems.com>
2021-03-06 11:22:12 +0000

    box of hope form working with emailjs, just not datepicker - look to correct

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,21 +1,52 @@
 import React, {useState} from "react";
-import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import {useForm, Controller} from "react-hook-form";
 import "../styles/Box.css";
+import {init, sendForm} from "emailjs-com";
+init("user_UJZWlkJrwfOaaqvPbKAZa");
 
 const Box = () => {
-  const [boxKey, setBoxKey] = useState(10);
+  const [contactNumber, setContactNumber] = useState("000000");
+  const [statusMessage, setStatusMessage] = useState("Message");
+
+  const generateContactNumber = () => {
+    const numStr = "000000" + ((Math.random() * 1000000) | 0);
+    setContactNumber(numStr.substring(numStr.length - 6));
+  };
+
   const preloadedValues = {
     babiesAge: "N/A",
   };
-  const {handleSubmit, register, control, errors} = useForm({
+
+  const {register, handleSubmit} = useForm({
     defaultValues: preloadedValues,
   });
 
   const onSubmit = (data) => {
     console.log(data);
-    setBoxKey(boxKey + 1);
+    const statusMessage = document.querySelector(".status-message");
+    const form = document.querySelector("#box-of-hope");
+    generateContactNumber();
+
+    sendForm("default_service", "box_of_hope", "#box-of-hope").then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        form.reset();
+        setStatusMessage("Message sent!");
+        statusMessage.className = "status-message success";
+        setTimeout(() => {
+          statusMessage.className = "status-message";
+        }, 5000);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        setStatusMessage("Failed to send message! Please try again later.");
+        statusMessage.className = "status-message failure";
+        setTimeout(() => {
+          statusMessage.className = "status-message";
+        }, 5000);
+      }
+    );
   };
 
   return (
@@ -25,7 +56,8 @@ const Box = () => {
         Fill in the below form to refer a friend, family member or yourself for
         a little box of hope.
       </p>
-      <form key={boxKey} onSubmit={handleSubmit(onSubmit)}>
+      <p className="status-message">{statusMessage}</p>
+      <form id="box-of-hope" onSubmit={handleSubmit(onSubmit)}>
         <div className="box-form-section">
           <label htmlFor="name" className="form-label">
             Name
@@ -34,11 +66,9 @@ const Box = () => {
             id="name"
             name="name"
             type="text"
-            ref={register({
-              required: {message: "This is a required field", value: true},
-            })}
+            required={true}
+            ref={register}
           />
-          {errors.name && <span className="error">{errors.name.message}</span>}
         </div>
         <div className="box-form-section">
           <label htmlFor="email" className="form-label">
@@ -48,13 +78,9 @@ const Box = () => {
             id="email"
             name="email"
             type="text"
-            ref={register({
-              required: {message: "This is a required field", value: true},
-            })}
+            required={true}
+            ref={register}
           />
-          {errors.email && (
-            <span className="error">{errors.email.message}</span>
-          )}
         </div>
         <div className="box-form-section">
           <label htmlFor="phone" className="form-label">
@@ -64,30 +90,20 @@ const Box = () => {
             id="phone"
             name="phone"
             type="number"
-            ref={register({
-              required: {message: "This is a required field", value: true},
-            })}
+            required={true}
+            ref={register}
           />
-          {errors.phone && (
-            <span className="error">{errors.phone.message}</span>
-          )}
         </div>
         <div className="box-form-section">
-          <label htmlFor="birthDate" className="form-label">
+          <label htmlFor="date_of_birth" className="form-label">
             Date of birth
           </label>
-          <Controller
-            control={control}
-            name="birthDate"
-            render={({onChange, onBlur, value}) => (
-              <ReactDatePicker
-                onChange={onChange}
-                onBlur={onBlur}
-                selected={value}
-                dateFormat="dd/MM/yyyy"
-                placeholderText="DD/MM/YYYY"
-              />
-            )}
+          <input
+            id="date_of_birth"
+            name="date_of_birth"
+            type="text"
+            required={true}
+            ref={register}
           />
         </div>
         <div className="box-form-section">
@@ -97,46 +113,33 @@ const Box = () => {
           <select
             id="communication"
             name="communication"
-            ref={register({
-              required: {
-                message: "This is a required field",
-                value: true,
-              },
-            })}
+            required={true}
+            ref={register}
           >
             <option value="">Select...</option>
             <option value="Phone Call">Phone call</option>
             <option value="Text">Text</option>
             <option value="Email">Email</option>
           </select>
-          {errors.communication && (
-            <span className="error">{errors.communication.message}</span>
-          )}
         </div>
         <h1>Your pregnancy</h1>
         <div className="box-form-section">
-          <label htmlFor="dueDate" className="form-label">
+          <label htmlFor="due_date" className="form-label">
             Due date (where applicable)
           </label>
-          <Controller
-            control={control}
-            name="dueDate"
-            render={({onChange, onBlur, value}) => (
-              <ReactDatePicker
-                onChange={onChange}
-                onBlur={onBlur}
-                selected={value}
-                dateFormat="dd/MM/yyyy"
-                placeholderText="DD/MM/YYYY"
-              />
-            )}
+          <input
+            id="due_date"
+            name="due_date"
+            type="text"
+            required={true}
+            ref={register}
           />
         </div>
         <div className="box-form-section">
-          <label htmlFor="babiesAge" className="form-label">
+          <label htmlFor="babies_age" className="form-label">
             Babies age (where applicable)
           </label>
-          <input id="babiesAge" name="babiesAge" type="text" ref={register} />
+          <input id="babies_age" name="babies_age" type="text" ref={register} />
         </div>
         <div className="box-form-section">
           <label htmlFor="midwife" className="form-label">
@@ -146,55 +149,36 @@ const Box = () => {
             id="midwife"
             name="midwife"
             type="text"
-            ref={register({
-              required: {message: "This is a required field", value: true},
-            })}
+            required={true}
+            ref={register}
           />
-          {errors.midwife && (
-            <span className="error">{errors.midwife.message}</span>
-          )}
         </div>
         <div className="box-form-section">
           <label htmlFor="referral" className="form-label">
             Referral type
           </label>
-          <select
-            id="referral"
-            name="referral"
-            ref={register({
-              required: {
-                message: "This is a required field",
-                value: true,
-              },
-            })}
-          >
+          <select id="referral" name="referral" required={true} ref={register}>
             <option value="">Select...</option>
             <option value="Professional">Professional</option>
             <option value="Friend">Friend</option>
             <option value="Family member">Family member</option>
             <option value="Self">Self</option>
           </select>
-          {errors.referral && (
-            <span className="error">{errors.referral.message}</span>
-          )}
         </div>
         <div className="box-form-section">
-          <label htmlFor="hearAbout" className="form-label">
+          <label htmlFor="hear_about" className="form-label">
             How did you hear about us?
           </label>
           <input
-            id="hearAbout"
-            name="hearAbout"
+            id="hear_about"
+            name="hear_about"
             type="text"
-            ref={register({
-              required: {message: "This is a required field", value: true},
-            })}
+            required={true}
+            ref={register}
           />
-          {errors.hearAbout && (
-            <span className="error">{errors.hearAbout.message}</span>
-          )}
         </div>
-        <button type="submit">Submit</button>
+        <input className="button" type="submit" value="Send" />
+        <input type="hidden" name="contact_number" value={contactNumber} />
       </form>
     </div>
   );
