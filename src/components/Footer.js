7dffdcffe5956af4e4f82a35dc146ba5352bdec1
“Joe <josephbramwell@bolstersystems.com>
2021-03-21 19:28:35 +0000

    style tweeks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,25 @@
 import React from "react";
+import {animateScroll} from "react-scroll";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowAltCircleUp} from "@fortawesome/free-solid-svg-icons";
 import "../styles/Footer.css";
 
 const Footer = () => {
+  const handleScrollToTop = () => {
+    animateScroll.scrollToTop();
+  };
   return (
     <div className="footer">
-      <p>this is my footer</p>
+      <p className="footer-p">
+        Created by{" "}
+        <a href="https://github.com/jbram-js" target="_blank" className="bram">
+          Bram
+        </a>
+      </p>
+
+      <span className="scroll-button" onClick={() => handleScrollToTop()}>
+        <FontAwesomeIcon icon={faArrowAltCircleUp} />
+      </span>
     </div>
   );
 };
